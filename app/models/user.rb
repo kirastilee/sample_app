c44b8c08c9a553fb15cba2b4cb2ba03e89a246af
khang <kira.stilee@gmail.com>
2020-12-28 18:55:34 +0700

    modelling user & sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,17 @@
 class User< ApplicationRecord
     validates :name, presence: true, length: { maximum:50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, length: { minimum: 6}
+    validates :email, presence: true, length: { minimum: 6},
                         format: {with: VALID_EMAIL_REGEX },
-                        unniqueness: { case_sensitive: false}
+                        uniqueness: { case_sensitive: false}
     validates :password, presence: true, length: {minimum: 6 }
 
-    before_save_password
+    before_save :email_downcase
 
     has_secure_password
 
     private
-        def
-            email_downcase
-            email.email_downcase
+        def email_downcase
+            email.downcase!
         end
 end
