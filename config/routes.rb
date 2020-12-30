1a466c1d93cb01855e4d494209f5674e9ed44123
khang <kira.stilee@gmail.com>
2020-12-30 21:19:16 +0700

    advanced_login

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 	get "/help", to:"static_pages#help"
 	get "/about", to:"static_pages#about"
 	resources :users
-	delete"/layout", to: "sessions#destroy"
+	delete "/logout", to: "sessions#destroy"
 	get "signup" , to: "users#new"
 	post "/login", to: "sessions#create" 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
