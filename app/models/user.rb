f113a6a0ae7e8c8935b09737530a2b63f07577a4
khang <kira.stilee@gmail.com>
2021-01-15 18:20:56 +0700

    update_fl_ures

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,20 @@
 class User< ApplicationRecord
     attr_accessor :remember_token
 
+    has_one_attached :image
+
+    has_many :active_relationships, class_name: "Relationship",
+                                    foreign_key: "follower_id",
+                                    dependent:
+                                    :destroy
+    
+    has_many :passive_relationships, class_name: "Relationship",
+                                    foreign_key: "followed_id",
+                                    dependent:
+                                    :destroy
+    has_many :following, through: :active_relationships, source: :followed
+    has_many :followers, through: :passive_relationships, source: :follower
+
     validates :name, presence: true, length: { maximum:50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { minimum: 6},
@@ -15,15 +29,15 @@ class User< ApplicationRecord
     has_many :microposts, dependent: :destroy
 
     class << self
-        def digest(string)
-          cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                        BCrypt::Engine.cost
-          BCrypt::Password.create(string, cost: cost)
-        end
+      def digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                      BCrypt::Engine.cost
+        BCrypt::Password.create(string, cost: cost)
+      end
     
-        def new_token
-          SecureRandom.urlsafe_base64
-        end
+      def new_token
+        SecureRandom.urlsafe_base64
+      end
     end
 
     def remember
@@ -36,7 +50,7 @@ class User< ApplicationRecord
     end
     
     def forget
-        update_attributes(remember_digest: nil)
+      update_attributes(remember_digest: nil)
     end
 
     def feed
@@ -47,8 +61,20 @@ class User< ApplicationRecord
       image.variant(resize_to_limit: [500, 500])
     end
 
+    def follow(other_user)
+      following << other_user
+    end
+
+    def unfollow(other_user)
+      following.delete(other_user)
+    end
+
+    def following?(other_user)
+      following.include?(other_user)
+    end
+
     private
     def email_downcase
-        email.downcase!
+      email.downcase!
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
-  	get '/login' , to: "sessions#new"
+  get '/login' , to: "sessions#new"
 	root to: "static_pages#home"
 	get "/help", to:"static_pages#help"
 	get "/about", to:"static_pages#about"
-	resources :users
 	delete "/logout", to: "sessions#destroy"
 	get "signup" , to: "users#new"
-	post "/login", to: "sessions#create" 
+  post "/login", to: "sessions#create" 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :microposts, only: [:create, :destroy]
+  resources :users do
+	  member do
+	    get :following, :followers
+    end
+  end
+  resources :relationships, only: [:create, :destroy]
 end
