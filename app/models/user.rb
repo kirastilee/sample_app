bede92e219af1902bdcf3183a0de5283e065a14c
khang <kira.stilee@gmail.com>
2021-01-18 18:58:58 +0700

    Following_users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,80 +1,71 @@
 class User< ApplicationRecord
-    attr_accessor :remember_token
+  has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                                  foreign_key: "followed_id",
+                                  dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
-    has_one_attached :image
+  attr_accessor :remember_token
 
-    has_many :active_relationships, class_name: "Relationship",
-                                    foreign_key: "follower_id",
-                                    dependent:
-                                    :destroy
-    
-    has_many :passive_relationships, class_name: "Relationship",
-                                    foreign_key: "followed_id",
-                                    dependent:
-                                    :destroy
-    has_many :following, through: :active_relationships, source: :followed
-    has_many :followers, through: :passive_relationships, source: :follower
+  validates :name, presence: true, length: { maximum:50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { minimum: 6},
+                      format: {with: VALID_EMAIL_REGEX },
+                      uniqueness: { case_sensitive: false}
+  validates :password, presence: true, length: {minimum: 6 }#, allow_nil: true
 
-    validates :name, presence: true, length: { maximum:50 }
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, length: { minimum: 6},
-                        format: {with: VALID_EMAIL_REGEX },
-                        uniqueness: { case_sensitive: false}
-    validates :password, presence: true, length: {minimum: 6 }, allow_nil: true
+  before_save :email_downcase
 
-    before_save :email_downcase
-
-    has_secure_password
-
-    has_many :microposts, dependent: :destroy
-
-    class << self
+  has_secure_password
+  
+  class << self
       def digest(string)
         cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                       BCrypt::Engine.cost
         BCrypt::Password.create(string, cost: cost)
       end
-    
+  
       def new_token
         SecureRandom.urlsafe_base64
       end
-    end
+  end
 
-    def remember
-      self.remember_token =User.new_token
+  def remember
+      self.remember_token = User.new_token
       update_attributes(remember_digest: User.digest(remember_token))
-    end
+  end
 
-    def authenticated?(remember_token)
-      BCrypt::Password.new(self.remember_digest).is_password?(remember_token)
-    end
-    
-    def forget
-      update_attributes(remember_digest: nil)
-    end
+  def authenticated?(remember_token)
+      BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 
-    def feed
-      microposts
-    end 
+  def forget
+      update_attribute(:remember_digest,nil)
+  end
 
-    def display_image
-      image.variant(resize_to_limit: [500, 500])
-    end
+  def feed
+    Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
+  end
 
-    def follow(other_user)
+  def follow(other_user)
       following << other_user
-    end
+  end
 
-    def unfollow(other_user)
+  def unfollow(other_user)
       following.delete(other_user)
-    end
+  end
 
-    def following?(other_user)
+      # Returns true if the current user is following the other user.
+  def following?(other_user)
       following.include?(other_user)
-    end
+  end
 
-    private
-    def email_downcase
+  private
+  def email_downcase
       email.downcase!
-    end
-end
+  end  
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,20 @@
 Rails.application.routes.draw do
-  get '/login' , to: "sessions#new"
+	get "/login", to: 'sessions#new'
+	delete "/logout", to: "sessions#destroy"
 	root to: "static_pages#home"
 	get "/help", to:"static_pages#help"
 	get "/about", to:"static_pages#about"
-	delete "/logout", to: "sessions#destroy"
-	get "signup" , to: "users#new"
-  post "/login", to: "sessions#create" 
+	resources :users
+	get "/signup", to: "user#new"
+	post "login", to: "sessions#create"
+	resources :users do
+		member do
+			get :following, :followers
+		end
+	end
+	resources :account_activations, only: [:edit]
+	resources :password_resets, only: [:new, :create, :edit, :update]
+	resources :microposts, only: [:create, :destroy]
+	resources :relationships, only: [:create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :microposts, only: [:create, :destroy]
-  resources :users do
-	  member do
-	    get :following, :followers
-    end
-  end
-  resources :relationships, only: [:create, :destroy]
 end
